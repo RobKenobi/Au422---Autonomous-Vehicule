d515908b122abab5bde170ea85d1bd19d9ca3bee
Rob_V <robinson.vaitilingom@outlook.fr>
2022-05-11 22:43:34 +0200

    RRT fonctionne pas opti encore

--- scripts/rrt_dev/Path.py
+++ scripts/rrt_dev/Path.py
@@ -1,5 +1,5 @@
 from Node import Node
-from Map import DEFAULT_MAP
+from Map import DEFAULT_MAP1
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,9 +13,8 @@ def saturation(value, maximum, minimum=0):
 
 
 class Path:
-    i = 0
 
-    def __init__(self, init_node=Node(), goal_node=Node(), map_env=DEFAULT_MAP, dq=2, robot_size=3, max_iter=1000):
+    def __init__(self, init_node=Node(), goal_node=Node(), map_env=DEFAULT_MAP1, dq=2, robot_size=3, max_iter=1000):
         if isinstance(init_node, Node):
             self.init_node = init_node
         else:
@@ -29,7 +28,7 @@ class Path:
         try:
             if len(map_env.shape) == 2:
                 self.map = map_env
-                self.plot_map = map_env
+                self.plot_map = self.map.copy()
             else:
                 raise ValueError("map_env must be a 2D-array")
         except:
@@ -65,11 +64,13 @@ class Path:
         y_max = y + self.robot_size + 1
 
         if x_min < 0 or y_min < 0 or x_max > width or y_max > height:
+            print(1, x, y)
             return False
 
         square = self.map[x_min:x_max, y_min:y_max]
 
         if np.sum(square):
+            print(f"2 -> x:{x}\ty:{y}\n{square}")
             return False
         return True
 
@@ -89,7 +90,7 @@ class Path:
             b = x1 - a * y1
             to_check_x = [int(a * y_i + b) for y_i in to_check_y]
         else:  # Meaning (x1,y1) == (x2,y2)
-            return None
+            return False
 
         for x, y in zip(to_check_x, to_check_y):
             if not self.free_square(x, y):
@@ -145,7 +146,6 @@ class Path:
                 node = new_node
                 self.list_nodes.append(node)
 
-
         if self.max_iter:
             while node.getParent() is not None:
                 self.original_path.append(node.getPos())
@@ -155,7 +155,7 @@ class Path:
             if plot:
                 x = [self.init_node.getPos('x')]
                 y = [self.init_node.getPos('y')]
-                self.plot_map[x,y] = 3
+                self.plot_map[x, y] = 3
                 for x_i, y_i in self.original_path:
                     x.append(x_i)
                     y.append(y_i)
@@ -189,7 +189,6 @@ map = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 if __name__ == "__main__":
     init = Node((1, 1))
     goal = Node((17, 17))
-    P = Path(init, goal, map_env=DEFAULT_MAP, dq=4, robot_size=3, max_iter=100000)
+    P = Path(init, goal, map_env=DEFAULT_MAP1, dq=6, robot_size=1, max_iter=1000)
     P.generate(optimize=False, plot=True)
     print(P.getPath(optimize=False))
-    print(P.list_nodes)
