82c2036474285e30e4cd02b870fdd22ac9381172
Jojo <j.granier@hotmail.fr>
2022-06-01 19:32:35 +0000

    Woulah j'ai essayé

--- scripts/agent.py
+++ scripts/agent.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import math
+
 import rospy
 
 from geometry_msgs.msg import Pose2D, Twist
@@ -13,11 +15,12 @@ class Agent:
         self.listener = tf.TransformListener()
 
         self.robot_pose = Pose2D()
-        self.pos = self.robot_pose[:2]
-        self.orient = self.robot_pose[-1]
+        self.pos = (self.robot_pose.x, self.robot_pose.y)
+        self.orient = self.robot_pose.theta
         self.goal_received, self.reached = False, False
 
-        self.path_sub = rospy.Subscriber("path", Path, self.plannerCb, queue_size=1)
+        self.path_sub = rospy.Subscriber(
+            "path", Path, self.plannerCb, queue_size=1)
         self.vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
         self.timer_pose = rospy.Timer(rospy.Duration(0.5), self.poseCb)
         self.timer_follower = rospy.Timer(rospy.Duration(0.1), self.moveToGoal)
@@ -29,27 +32,68 @@ class Agent:
     def poseCb(self, event):
         """ Get the current position of the robot each 500ms """
         try:
-            trans, rot = self.listener.lookupTransform("/map", "/base_footprint", rospy.Time(0))
+            trans, rot = self.listener.lookupTransform(
+                "/map", "/base_footprint", rospy.Time(0))
             self.robot_pose.x = trans[0]
             self.robot_pose.y = trans[1]
-            self.robot_pose.theta = tf.transformations.euler_from_quaternion(rot)[2]
-            print(f"Robot's pose: {self.robot_pose.x:.2f}, {self.robot_pose.y:.2f}, {self.robot_pose.theta:.2f}")
+            self.pos = (self.robot_pose.x, self.robot_pose.y)
+            self.orient = self.robot_pose.theta
+            self.robot_pose.theta = tf.transformations.euler_from_quaternion(rot)[
+                2]
+            print(
+                f"Robot's pose: {self.robot_pose.x:.2f}, {self.robot_pose.y:.2f}, {self.robot_pose.theta:.2f}")
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             print("Could not transform /base_footprint to /map")
 
     def plannerCb(self, msg):
         self.reached, self.goal_received = False, True
         list_PoseStamped = msg.poses
-        self.path = list(map(lambda pose: pose.position[:2], list_PoseStamped))
+        self.path = list(
+            map(lambda pose: (pose.pose.position.x, pose.pose.position.y), list_PoseStamped))
 
         # Remove robot position if in the list
-        if self.path[0] == self.pos:
-            self.path.pop(0)
+        if len(self.path) == 0:
+            self.reached = True
+            self.linear = 0
+            self.angular = 0
+            self.send_velocities()
+            print("The goal has been reached")
+        else:
+            try:
+                if math.dist(self.path[0], self.pos) < 0.1:
+                    self.path.pop(0)
+                    self.linear = 0
+                    self.angular = 0
+                    self.send_velocities()
+                    print("STOP")
+            except:
+                pass
 
     def moveToGoal(self, event):
         if not self.reached and self.goal_received:
-            pass
-            # Add your strategy here
+            goal = self.path[0]
+
+            # Strategy
+
+            # We import the actual position of the robot
+            [x, y, yaw] = [self.robot_pose.x,self.robot_pose.y, self.robot_pose.theta]
+
+            # We calculate the relative position of the goal and its distance
+            dx = goal[0]-x
+            dy = goal[1]-y
+            dist = (dx**2 + dy**2)**0.5
+
+            # We calculate the angle between the reference of the robot and the goal
+            ob = math.atan2(dy, dx)
+
+            #    We calculate the angular velocity by using the angle of the robot and the angle of
+            # the objective in the robot reference
+            self.angular = ob-yaw
+            self.linear = 2*dist/5 + 0.5
+
+            # End of strategy
+
+            self.send_velocities()
 
     def send_velocities(self):
         self.linear = self.constraint(self.linear, minimum=-2.0, maximum=2.0)
--- scripts/path_planning_rrt.py
+++ scripts/path_planning_rrt.py
@@ -79,7 +79,7 @@ class RRT:
             self.robot_pose.y = trans[1]
             self.pos = (self.robot_pose.x, self.robot_pose.y)
 
-            print(f"Robot's pose: {self.robot_pose.x}, {self.robot_pose.y}")
+            #print(f"Robot's pose: {self.robot_pose.x:.2f}, {self.robot_pose.y:.2f}, {self.robot_pose.theta:.2f}")
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             print("Could not transform /base_footprint to /map")
 
@@ -117,8 +117,8 @@ class RRT:
         max_iter = self.max_iter
 
         P = rr.Path(init_node, goal_node, _map, dq, robot_size, max_iter)
-        P.generate(optimize=True, smooth=True)
-        self.path = P.getPath("smooth",init=True)
+        P.generate(optimize=False, smooth=False)
+        self.path = P.getPath("original",init=True)
         print(self.path, file=sys.stderr)
 
         self.publishPath()
